05d4d7a0325ea0493bd4ad5fc5935ba834384444
OneAbowAll <42043741+OneAbowAll@users.noreply.github.com>
2020-03-17 21:43:30 +0100

    Base project

---
+++ Assets/Scripts/ChunkManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ChunkManager : ScriptableObject
+{
+    public Chunk chunkPrefab;
+    public int viewDistance;
+}
---
+++ Assets/Scripts/Editor/WorldEditor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(World))]
+public class WorldEditor : Editor
+{
+    World world;
+    Editor noiseEditor;
+    Editor chunkEditor;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Refresh World"))
+            world.RefreshWorld();
+
+        DrawNoiseSettingsEditor(world.RefreshWorld, ref world.noiseSettingsFoldout, ref noiseEditor);
+        DrawChunkManagerSettingsEditor(ref world.chunkSettingsFoldout, ref noiseEditor);
+    }
+
+    void DrawNoiseSettingsEditor(System.Action onSettingsUpdated, ref bool foldout, ref Editor editor)
+    {
+        Object settings = world.noiseSettings;
+        if (settings == null)
+            return;
+
+        foldout = EditorGUILayout.InspectorTitlebar(foldout, settings);
+        using (var check = new EditorGUI.ChangeCheckScope())
+        {
+            if (foldout)
+            {
+                CreateCachedEditor(settings, null, ref editor);
+                editor.OnInspectorGUI();
+               
+
+                if (check.changed)
+                {
+                    if (onSettingsUpdated != null && world.autoRefresh)
+                    {
+                        onSettingsUpdated();
+                    }
+                }
+            }
+        }
+    }
+
+    void DrawChunkManagerSettingsEditor(ref bool foldout, ref Editor editor)
+    {
+        Object settings = world.chunkManager;
+        if (settings == null)
+            return;
+
+        foldout = EditorGUILayout.InspectorTitlebar(foldout, settings);
+        using (var check = new EditorGUI.ChangeCheckScope())
+        {
+            if (foldout)
+            {
+                CreateCachedEditor(settings, null, ref editor);
+                editor.OnInspectorGUI();
+            }
+        }
+    }
+
+
+    void OnEnable()
+    {
+        world = (World)target;
+    }
+}
